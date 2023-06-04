95955158a9f3a27e03d74e82161a75ec69fb76fa
LauraMemedovic <77235878+LauraMemedovic@users.noreply.github.com>
2023-06-04 22:19:15 +0200

    Update App.js

--- App.js
+++ App.js
@@ -1,21 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+import rijecReducer from './store/reducers/rijeci';
+
+import Igra from './screens/Igra';
+import Pocetni from './screens/Pocetni';
+import Kraj from './screens/Kraj';
+import Naslov from './components/Naslov';
+import Botun from './components/Botun';
+import { RIJECI } from './data/podaci';
+
+const Stack = createNativeStackNavigator();
+
+const glavniReducer = combineReducers({
+  rijeci: rijecReducer,
+});
+
+const store = createStore(glavniReducer);
 
 export default function App() {
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider>
+    </Provider>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  
 });
+
