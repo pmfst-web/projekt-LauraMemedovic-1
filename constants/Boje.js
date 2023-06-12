3aefe99d82dc6221449c13344538a8f9a93c498b
LauraMemedovic <77235878+LauraMemedovic@users.noreply.github.com>
2023-06-12 12:53:12 +0200

    dodane Boje

---
+++ constants/Boje.js
@@ -0,0 +1,9 @@
+export default {
+  Bijela: '#FFFFF6',
+  Primarna: '#00006B',
+  Naglasak1: '#F9BA8F',
+  Naglasak2: '#F9AB76',
+  Pozadina: '#BAE8E8',
+  inputBox: '#6B718044',
+  wordBox: 'powderblue',
+};
