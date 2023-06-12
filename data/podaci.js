5d99699d7956e43ca1ad997af6fd8833f1b5d744
LauraMemedovic <77235878+LauraMemedovic@users.noreply.github.com>
2023-06-12 12:52:36 +0200

    Delete data directory

--- data/podaci.js
+++
@@ -1,9 +0,0 @@
-import Rijec from '../models/rijec'
-
-export const RIJECI = [
-  new Rijec(0, "Ana"),
-  new Rijec(1, "Ivan"),
-  new Rijec(2, "Matea"),
-  new Rijec(3, "Petar"),
-  new Rijec(4, "Iva"),
-]
