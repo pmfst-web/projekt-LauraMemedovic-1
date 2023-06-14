e428484694bca8fca0f597110a85949ec0c47307
LauraMemedovic <77235878+LauraMemedovic@users.noreply.github.com>
2023-06-14 08:26:47 +0200

    Update App.js

--- App.js
+++ App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, combineReducers } from 'redux';
 import gameReducer from './store/reducers/gameReducer';
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
 import HomeScreen from './screens/HomeScreen';
 import GameScreen from './screens/GameScreen';
+import EndScreen from './screens/EndScreen';
 
 const Stack = createStackNavigator();
 
+//stvaranje centralnog spremnika
 const store = createStore(gameReducer) 
 
 const App = () => {
@@ -18,6 +20,7 @@ const App = () => {
         <Stack.Navigator>
           <Stack.Screen name="Pravila" component={HomeScreen} />
           <Stack.Screen name="Igra" component={GameScreen} />
+          <Stack.Screen name="Kraj" component={EndScreen} />
         </Stack.Navigator>
       </NavigationContainer>
     </Provider>
