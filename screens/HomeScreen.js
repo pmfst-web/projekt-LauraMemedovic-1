a0aeeac1dfa0e424850cee62515650b41dfff904
LauraMemedovic <77235878+LauraMemedovic@users.noreply.github.com>
2023-06-19 22:29:31 +0200

    Update HomeScreen.js - težine

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,24 +1,41 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { StyleSheet, View, Button, Text, Image } from 'react-native';
-import Boje from '../constants/Boje';
 
 const HomeScreen = ({ navigation }) => {
+  const [tezina, setTezina] = useState('lagano');
+
   const startGame = () => {
-    navigation.navigate('Igra');
+    navigation.navigate('Igra', {tezina});
+  };
+
+  const handleTezinaChange = (novaTezina) => {
+    setTezina(novaTezina);
   };
 
   return (
     <View style={styles.container}>
-      <View style={styles.slikaOkvir}>
-        <Image style={styles.slika} source={require('../assets/game2.png')} />
-      </View>
       <Text style={styles.tekst}>
         {'Cilj igre je pogoditi skrivenu riječ.'+
-        ' \n Imaš 10 pokušaja da pogodiš.'+
+        ' \n Odaberi težinu igre:'+
+        '\n LAGANO: 10 života' +
+        '\n SREDNJE: 7 života' +
+        '\n TEŠKO: 5 života' +
         ' \n Svaki put kad upišeš krivo slovo gubiš život.'+
         ' \n Igra je gotova kad pogodiš riječ ili kad izgubiš sve živote.'}
       </Text>
-      <View sytle={styles.tipka}>
+      <View style={styles.tipkeContainer}>
+        <Button title="LAGANO" onPress={() => handleTezinaChange('lagano')} />
+        <Button title="SREDNJE" onPress={() => handleTezinaChange('srednje')} />
+        <Button title="TEŠKO" onPress={() => handleTezinaChange('teško')} />
+      </View>
+      {tezina && (
+      <View>
+        <Text style={styles.tekst}>
+          Odabrana težina: {tezina}
+        </Text>
+      </View>
+    )}
+      <View>
         <Button title="ZAPOČNI IGRU" onPress={startGame} />
       </View>
     </View>
@@ -28,40 +45,29 @@ const HomeScreen = ({ navigation }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: 'top',
     alignItems: 'center',
-  },
-  slika: {
-    width: 350,
-    height: 100,
-    flex: 1,
-  },
-  slikaOkvir: {
-    overflow: 'hidden',
-    width: '100%',
-    height: 400,
-    borderRadius: 10,
-    marginVertical: 10,
   },
   tekst: {
     color: 'black',
     padding: 20,
     marginTop: 2,
+    marginBottom: 20,
     fontSize: 20,
     paddingTop: 0,
     textAlign: 'center',
     fontFamily: 'comfortaa',
-    backgroundColor: '#9ebdf0',
+    backgroundColor: '#dae7f0',
     borderWidth: 4,
     borderColor: 'white',
     lineHeight: 30,
   },
-  tipka: {
-    padding: 10,
-    width: 200,
-    alignItems: 'center',
-    backgroundColor: '#9ebdf0',
+  tipkeContainer: {
+    flexDirection: 'row', 
+    justifyContent: 'space-evenly', 
+    width: '100%',
     marginTop: 25,
+    marginBottom: 25,
   },
 });
 
