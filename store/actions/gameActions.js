5bdd83b411a20312735b3dcad96439c698c2de73
LauraMemedovic <77235878+LauraMemedovic@users.noreply.github.com>
2023-06-20 00:25:11 +0200

    Update gameActions.js

--- store/actions/gameActions.js
+++ store/actions/gameActions.js
@@ -1,20 +1,38 @@
 import { START_GAME, POGODI_SLOVO } from '../types';
 
-import {getRandomWord} from '../podaci/rijeci'
+import { getRandomMovie, getRandomSeries } from '../podaci/rijeci';
 
-export const startGame = (brojZivota) => {
-  const randomWord = getRandomWord();
+export const startGame = (brojZivota, odabranaKategorija) => {
+  let randomWord = '';
+  let pogodenaRijec = '';
+
+  if (odabranaKategorija === 'filmovi') {
+    randomWord = getRandomMovie();
+  } else if (odabranaKategorija === 'serije') {
+    randomWord = getRandomSeries();
+  }
+
+  for (let i = 0; i < randomWord.length; i++) {
+    if (randomWord[i] === ' ') {
+      pogodenaRijec += ' '; //razmak ako je u riječi razmak
+    } else {
+      pogodenaRijec += '_'; //underscore ako je slovo
+    }
+  }
+
   return {
     type: START_GAME,
     payload: {
+      kategorija: odabranaKategorija,
       skrivenaRijec: randomWord,
-      pogodenaRijec: '_'.repeat(randomWord.length),
+      pogodenaRijec,
       zivoti: brojZivota,
     },
   };
 };
 
-export const pogodiSlovo= slovo => {
+
+export const pogodiSlovo = (slovo) => {
   const s = slovo.toUpperCase();
   return {
     type: POGODI_SLOVO,
