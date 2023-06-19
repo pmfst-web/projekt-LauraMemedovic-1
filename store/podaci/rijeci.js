19b0d1eb9f463d44f3f794666692e7946e987ac4
LauraMemedovic <77235878+LauraMemedovic@users.noreply.github.com>
2023-06-20 00:25:29 +0200

    Update rijeci.js

--- store/podaci/rijeci.js
+++ store/podaci/rijeci.js
@@ -1,8 +1,56 @@
-const wordList = ['kuća', 'pas', 'mačka', 'drvo', 'stol', 'automobil', 'knjiga', 'sunce', 'more', 'trava', 'cvijet', 'brod', 'ptica', 'zrak', 'voda', 'planina', 'cesta', 'novac', 'šuma', 'oblak', 'torba', 'voće', 'povrće', 'riba', 'zvijezda', 'mjesečina', 'kamen', 'igračka', 'rođendan', 'obitelj', 'prijatelj', 'ljubav', 'sreća', 'smijeh', 'putovanje', 'glazba', 'ples', 'sport', 'život', 'hrana', 'posao', 'vrijeme', 'grad', 'boja', 'jezik', 'misao', 'životinja', 'biljka', 'zvuk', 'miris', 'okus'];
+export const movieList = [
+  'Avatar',
+  'Superbad',
+  'The Prestige',
+  'Dark Knight',
+  'Avengers',
+  'Inception',
+  'Pulp Fiction',
+  'The Shawshank Redemption',
+  'Fight Club',
+  'The Godfather',
+  'Interstellar',
+  'The Matrix',
+  'Forrest Gump',
+  'Goodfellas',
+  'The Lion King',
+  'Titanic',
+  'Gladiator',
+  'The Avengers',
+  'Jurassic Park',
+  'Star Wars',
+];
 
-export const getRandomWord = () => {
-  const randomIndex = Math.floor(Math.random() * wordList.length);
-  return wordList[randomIndex];
+export const seriesList = [
+  'Breaking Bad',
+  'Shameless',
+  'The Sopranos',
+  'Fleabag',
+  'Better Call Saul',
+  'Game of Thrones',
+  'Friends',
+  'Stranger Things',
+  'The Office',
+  'The Crown',
+  'Black Mirror',
+  'Westworld',
+  'Narcos',
+  'The Big Bang Theory',
+  'Mindhunter',
+  'The Witcher',
+  'Money Heist',
+  'Peaky Blinders',
+  'The Mandalorian',
+  'The Walking Dead',
+];
+
+
+export const getRandomMovie = () => {
+  const randomIndex = Math.floor(Math.random() * movieList.length);
+  return movieList[randomIndex];
 };
 
-export default wordList;
+export const getRandomSeries = () => {
+  const randomIndex = Math.floor(Math.random() * seriesList.length);
+  return seriesList[randomIndex];
+};
